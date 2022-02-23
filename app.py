24e98b8f16eff38270cd73cdcbb28361c4d6bd16
Yoshishiro <40735530+Yoshishiro@users.noreply.github.com>
2022-02-23 18:00:24 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -26,10 +26,31 @@ def index():
         Age = float(request.form.get("Age"))
         Loan = float(request.form.get("Loan"))
         print(Income, Age, Loan)
-        model = joblib.load("XGBoost")
-        pred = model.predict([[Income, Age, Loan]])
-        s = "The credit card default is " + str(pred)
-        return(render_template("index.html", result = s))
+        model1 = joblib.load("Decision Tree")
+        pred1 = model1.predict([[Income, Age, Loan]])
+        s = "The credit card default is " + str(pred1) + "based on Decision Tree."
+        
+        model2 = joblib.load("XGBoost")
+        pred2 = model2.predict([[Income, Age, Loan]])
+        s1 = "The credit card default is " + str(pred2) + "based on XGBoost."
+        
+        model3 = joblib.load("Logistic Regression")
+        pred3 = model3.predict([[Income, Age, Loan]])
+        s2 = "The credit card default is " + str(pred3) + "based on Logistic Regression."        
+        
+        model4 = joblib.load("Neural Network")
+        pred4 = model4.predict([[Income, Age, Loan]])
+        s3 = "The credit card default is " + str(pred4) + "based on Neural Network."
+        
+        model5 = joblib.load("Random Forest")
+        pred5 = model5.predict([[Income, Age, Loan]])
+        s4 = "The credit card default is " + str(pred5) + "based on Random Forest."        
+
+        return(render_template("index.html",  result = s,
+                                              result1 = s1,
+                                              result2 = s2,
+                                              result3 = s3,
+                                              result4 = s4))
     else:
         return(render_template("index.html", result = "Try Again"))
 
